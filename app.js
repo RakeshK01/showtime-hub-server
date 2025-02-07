00ddf0364dbb4b2fbbeca990b591c17917d128d1
AbishekEchno77 <108268835+AbishekEchno77@users.noreply.github.com>
2025-02-07 17:42:15 +0530

    Added 3 APIs - country-list, movie-list, showtime-list

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 var app = express();
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
-var adminRoute=require('./routes/adminRoute');
+var adminRoute = require('./routes/adminRoute');
+var dashboardRoute = require('./routes/dashboardRoute');
 
 
 // view engine setup
@@ -21,14 +22,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/admin',adminRoute);
+app.use('/admin', adminRoute);
+app.use('/dashboard', dashboardRoute);
 // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
 //   next(createError(404));
 // });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- src/dashboard/dashboard.controller.js
+++ src/dashboard/dashboard.controller.js
@@ -3,6 +3,8 @@ const service = require('./dashboard.service');
 exports.getCountryList = async (req, res) => {
 
     try {
+        
+        let { limit, offset } = req.body
 
         let json = {
             code: 200,
@@ -10,7 +12,7 @@ exports.getCountryList = async (req, res) => {
             country_list: [],
         }
 
-        json.country_list = await service.getCountryList();
+        json.country_list = await service.getCountryList(limit, offset);
 
         return res.status(200).json(json)
 
@@ -32,13 +34,45 @@ exports.getMovieList = async (req, res) => {
 
     try {
 
+        let { limit, offset } = req.body
+      
         let json = {
             code: 200,
             message: "Success",
             movie_list: [],
         }
 
-        json.movie_list = await service.getMovieList();
+        json.movie_list = await service.getMovieList(limit, offset);
+
+        return res.status(200).json(json)
+
+    } catch (error) {
+
+        console.log("Catch error", error);
+
+        return res.status(500).json({
+            code: 500,
+            message: "Something went wrong",
+            error
+        })
+
+    }
+
+}
+
+exports.getMovieShowtimes = async (req, res) => {
+
+    try {
+
+        let { movie_id, country_id, limit, offset } = req.body
+
+        let json = {
+            code: 200,
+            message: "Success",
+            showtime_list: [],
+        }
+
+        json.showtime_list = await service.showtimeList(movie_id, country_id, limit, offset);
 
         return res.status(200).json(json)
 
